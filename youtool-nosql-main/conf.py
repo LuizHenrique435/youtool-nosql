01000b471a4e65f4b6f589341fe18164ef398beb
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2025-07-01 17:51:08 -0300

    d

--- youtool-nosql-main/conf.py
+++
@@ -1,3 +0,0 @@
-MONGO_URI = 'mongodb+srv://youtube_data:password1234@cluster0.c4i32dg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-DB_NAME = 'youtool'
-YOUTUBE_API_KEY = 'AIzaSyAmD1byi2vm7RComEHU5uTnj2o8T5GbotM'
